b721564308de5b9d38c43ba54472564b79f3e3a0
Yovo Manolov <yovo.manolov@abv.bg>
2025-02-10 14:25:28 +0200

    fixed liquibase migration

--- src/main/resources/db/changelog/data-population.sql
+++ src/main/resources/db/changelog/data-population.sql
@@ -1,4 +1,21 @@
+BEGIN;
 
+-- Reset sequence for products
+TRUNCATE products.product RESTART IDENTITY CASCADE;
+
+-- Reset sequence for customers
+TRUNCATE customers.customer RESTART IDENTITY CASCADE;
+
+-- Reset sequence for subscriptions
+TRUNCATE subscriptions.subscription RESTART IDENTITY CASCADE;
+
+-- Reset sequence for customer subscriptions
+TRUNCATE subscriptions.customer_subscription RESTART IDENTITY CASCADE;
+
+-- Reset sequence for promotions
+TRUNCATE promotions.promotion RESTART IDENTITY CASCADE;
+
+-- Insert data after resetting sequences
 INSERT INTO products.product (name, price, currency, discount_applied)
 VALUES
   ('IPhone 14 Pro', 1349, 'CHF', false),
@@ -9,9 +26,8 @@ VALUES
   ('MI 13', 899, 'CHF', true),
   ('Vivo X9', 1099, 'CHF', true);
 
--- Reset sequence for customers
 INSERT INTO customers.customer (name, address, gender, age, email_contact_number, product_id)
-VALUES 
+VALUES
  ('Customer1', 'addr1', 'M', 24, 'fname_lname1@email.com', 1),
  ('Customer2', 'addr2', 'F', 21, 'fname_lname2@email.com', 2),
  ('Customer3', 'addr3', 'M', 27, 'fname_lname3@email.com', 3),
@@ -20,9 +36,8 @@ VALUES
  ('Customer6', 'addr6', 'F', 36, 'fname_lname6@email.com', 6),
  ('Customer7', 'addr7', 'M', 38, 'fname_lname7@email.com', 7);
 
--- Reset sequence for subscriptions
-INSERT INTO subscriptions.subscription (name, price, currency ,subscription_type, validity)
-VALUES 
+INSERT INTO subscriptions.subscription (name, price, currency, subscription_type, validity)
+VALUES
  ('1GB30D', 2, 'CHF', 'prepaid', 'unlimited'),
  ('1GB30D', 3, 'CHF', 'prepaid', 'unlimited'),
  ('1GB30D', 5, 'CHF', 'prepaid', 'unlimited'),
@@ -30,16 +45,8 @@ VALUES
  ('ULData100M30D', 15, 'CHF', 'postpaid', '30 Days'),
  ('UL5G-ULMin30D', 30, 'CHF', 'postpaid', '30 Days');
 
-
---  Customer cannot by prepaid and postpaid subscriptions together
---  Customer can buy multiple prepaid
---  While replacing prepaid to postpaid, all prepaid should be removed.
---  At a time customer can buy only one prepaid
-
-
--- Reset sequence for customer_subscriptions
 INSERT INTO subscriptions.customer_subscription (customer_id, subscription_id)
-VALUES 
+VALUES
  (1, 1),
  (1, 2),
  (2, 5),
@@ -50,12 +57,12 @@ VALUES
  (6, 6),
  (7, 2);
 
--- Reset sequence for promotions
-INSERT INTO promotions.promotion (promocode, discount, discount_type, number_of_vouchers)
-VALUES 
+INSERT INTO promotions.promotion (promocode, discount, currency ,discount_type, number_of_vouchers)
+VALUES
  ('PROMO23', 2, 'CHF', 'prepaid', 'unlimited'),
  ('NEWYEAR', 15, 'CHF', 'prepaid', 'unlimited'),
  ('FLAT50', 50, 'CHF', 'prepaid', 'unlimited'),
  ('FLAT100', 100, 'CHF', 'prepaid', '30 days'),
  ('EASTER', 20, 'CHF', 'prepaid', '30 days');
 
+COMMIT;
